ff92c6696d224c0b5cd898ffbba06f7dcf0b06a3
jasurxayitmuradov <jxayitmuradov9@gmail.com>
2026-01-22 12:45:47 +0500

    fix: add legacy module declarations

---
+++ src/types/legacy-jsx.d.ts
@@ -0,0 +1,7 @@
+declare module './pages/Auth';
+declare module './pages/Profile';
+declare module './pages/Courses';
+declare module './pages/CourseDetail';
+declare module './pages/LessonPlayer';
+declare module './components/ProtectedRoute';
+declare module './lib/auth';
